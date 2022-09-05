6973948d3690861d4422f598057e58a6df228200
mantnicot <aldormilon@gmail.com>
2022-09-05 07:09:19 -0500

    Subida de cambios
    2-) Segunda parte de los cambios expuestos

--- pruebatecnica/src/main/java/cristian/net/pruebatecnica/repository/Descuentorepositorio.java
+++
@@ -1,7 +0,0 @@
-package cristian.net.pruebatecnica.repository;
-
-import cristian.net.pruebatecnica.modelo.Descuentos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Descuentorepositorio extends JpaRepository<Descuentos,Long> {
-}
